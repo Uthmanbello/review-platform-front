5116f7c2e2e45a30ff80b4a3ac2fbc71620a4770
Uthmanbello <uthmanbello88@gmail.com>
2023-11-11 21:05:58 +0100

    Style input field

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -56,35 +56,39 @@ const Facebook = () => {
 
 
   return (
-    <div style={{ width: '98%'}}>
-      <div ref={chatContainer} style={{ height: '65vh', overflowY: 'scroll', padding: '10px' }}>
-        {messages.map((message, index) => (
-          <div key={index} style={{ textAlign: message.isUser ? 'right' : 'left', margin: '5px' }}>
-            <div style={{ padding: '10px', display: 'inline-block', borderRadius: '10px', backgroundColor: message.isUser ? '#FDDAC1' : '#B0DAD9', color: message.isUser ? '#492a13' : '#083f3e' }}>
-              {message.text}
-            </div>
-          </div>
-        ))}
-      </div>
+    <>
+      <h2 className='page-heading'>FACEBOOK</h2>
 
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        {currentQuestion < questions.length && !allQuestionsAnswered && (
-          <input
-            type="text"
-            className='reply-input'
-            placeholder="Type your reply..."
-            onKeyPress={(e) => {
-              if (e.key === 'Enter') {
-                handleSendMessage(e.target.value);
-                e.target.value = '';
-              }
-            }}
-            style={{ width: '100%', marginTop: '10px', marginBottom: '30px', padding: '20px', backgroundColor: 'rgba(255, 240, 157, 0.87)', color: '#000000'}}
-          />
-        )}
-        {allQuestionsAnswered && <div className='reply-input' style={{ padding: '20px', marginTop: '10px', marginBottom: '30px', backgroundColor: 'rgba(255, 240, 157, 0.87)', color: '#000000' }}>All questions answered. We'll be in touch!</div>}
+      <div style={{ width: '75%', marginLeft: '15vw'}}>
+        <div ref={chatContainer} style={{ height: '65vh', overflowY: 'scroll', padding: '10px' }}>
+          {messages.map((message, index) => (
+            <div key={index} style={{ textAlign: message.isUser ? 'right' : 'left', margin: '5px' }}>
+              <div style={{ padding: '10px', display: 'inline-block', borderRadius: '10px', backgroundColor: message.isUser ? '#FDDAC1' : '#B0DAD9', color: message.isUser ? '#492a13' : '#083f3e' }}>
+                {message.text}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          {currentQuestion < questions.length && !allQuestionsAnswered && (
+            <input
+              type="text"
+              className='reply-input'
+              placeholder="Type your reply..."
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  handleSendMessage(e.target.value);
+                  e.target.value = '';
+                }
+              }}
+              style={{ width: '100%', marginTop: '10px', marginBottom: '30px', padding: '20px', backgroundColor: '#0c5e5c', color: '#ffffff'}}
+            />
+          )}
+          {allQuestionsAnswered && <div className='reply-input' style={{ padding: '20px', marginTop: '10px', marginBottom: '30px', backgroundColor: '#0c5e5c', color: '#ffffff' }}>All questions answered. We'll be in touch!</div>}
+        </div>
       </div>
-    </div>
+    </>
   )}
 
 export default Facebook;
