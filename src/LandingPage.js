80c1c5213780e0670b8c6a89a45d6d311e08f364
Uthmanbello <uthmanbello88@gmail.com>
2023-11-11 11:36:32 +0100

    Edit landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,12 +7,12 @@ const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className='landing'>
-        <h2 className='landing-page-heading'>My Lodge</h2>
+        <h2 className='landing-page-heading'>My Review Partner</h2>
         <div className='lang-btns'>
           <Link to="/chat" className='lang-btn english-btn'>Get Started</Link>
         </div>    
         <div className='landing-footer'>
-          <p style={{ color: '#ebbd90d5' }}>&copy; {new Date().getFullYear()} <span style={{ fontWeight: 500, fontFamily: 'Josefin Sans, sans-serif', color: '#ebbd90d5' }}>DeRoy<span style={{ color: '#ffb267' }} className="deroyale-at">@</span>l&#233;</span></p>
+          <p style={{ color: '#0F7876', fontWeight: '900' }}>&copy; {new Date().getFullYear()} <span style={{ fontWeight: 900, fontFamily: 'Josefin Sans, sans-serif', color: '#0F7876' }}>DeRoy<span style={{ color: '#F98F45' }} className="deroyale-at">@</span>l&#233;</span></p>
         </div>
       </div>
     </div>
