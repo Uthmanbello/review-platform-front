b06774320bebcff4eab529e0ef10b4f36d3c2ab3
Uthmanbello <uthmanbello88@gmail.com>
2023-11-11 21:09:54 +0100

    Edit reviews info

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,11 +3,11 @@ import '../styles/Info.css';
 
 const Info = () => {
   const reviewsData = [
-    { platform: 'FACEBOOK', bgColor: '#FDDAC1', textColor: '#F98F45', className: 'orange' },
-    { platform: 'TRIPADVISOR', bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
-    { platform: 'BOOKING', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
-    { platform: 'GOOGLE MY BUSINESS', bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
-    { platform: 'EXPEDIA', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
+    { platform: 'FACEBOOK', reviews: 125, replied: 120, pending: 5, bgColor: '#FDDAC1', textColor: '#F98F45', className: 'orange' },
+    { platform: 'TRIPADVISOR', reviews: 97, replied: 86, pending: 11, bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
+    { platform: 'BOOKING', reviews: 'N/A', replied: 'N/A', pending: 'N/A', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
+    { platform: 'GOOGLE MY BUSINESS', reviews: 'N/A', replied: 'N/A', pending: 'N/A', bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
+    { platform: 'EXPEDIA', reviews: 'N/A', replied: 'N/A', pending: 'N/A', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
   ];
 
   return (
@@ -22,9 +22,9 @@ const Info = () => {
           style={{ backgroundColor: review.bgColor, color: review.textColor }}
         >
           <h4 className={`review-heading ${review.className}`}>{review.platform}</h4>
-          <p className={review.className}><span className={review.className}>Reviews - </span> 125</p>
-          <p className={review.className}><span className={review.className}>Replied - </span> 120</p>
-          <p className={review.className}><span className={review.className}>Pending - </span> 5</p>
+          <p className={review.className}><span className={review.className}>Reviews - </span> {review.reviews}</p>
+          <p className={review.className}><span className={review.className}>Replied - </span> {review.replied}</p>
+          <p className={review.className}><span className={review.className}>Pending - </span> {review.pending}</p>
         </div>
       ))}
       {/* </div> */}
