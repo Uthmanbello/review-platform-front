ad6563e31b9c63129d46c24074f6b27b64cde7b7
Uthmanbello <uthmanbello88@gmail.com>
2023-11-11 20:46:45 +0100

    Adjust all the reviews

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import '../styles/Info.css'
+import '../styles/Info.css';
 
 const Info = () => {
+  const reviewsData = [
+    { platform: 'FACEBOOK', bgColor: '#FDDAC1', textColor: '#F98F45', className: 'orange' },
+    { platform: 'TRIPADVISOR', bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
+    { platform: 'BOOKING', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
+    { platform: 'GOOGLE MY BUSINESS', bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
+    { platform: 'EXPEDIA', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
+  ];
 
   return (
     <>
       <h2 className='page-heading'>INFO</h2>
-      {/* <div className='column'> */}
-        <div className='reviews' style={{ backgroundColor: '#FDDAC1', color: '#F98F45' }}>
-          <h4 className='review-heading orange'>FACEBOOK</h4>
-          <p className='orange'><span className='orange'>Reviews - </span> 125</p>
-          <p className='orange'><span className='orange'>Replied - </span> 120</p>
-          <p className='orange'><span className='orange'>Pending - </span> 5</p>
-        </div>
 
-        <div className='reviews' style={{ backgroundColor: '#B0DAD9', color: '#0F7876' }} >
-          <h4 className='review-heading green'>TRIPADVISOR</h4>
-          <p className='green'><span className='green'>Reviews - </span> 125</p>
-          <p className='green'><span className='green'>Replied - </span> 120</p>
-          <p className='green'><span className='green'>Pending - </span> 5</p>
+      {/* <div className=''> */}
+      {reviewsData.map((review, index) => (
+        <div
+          key={index}
+          className='reviews'
+          style={{ backgroundColor: review.bgColor, color: review.textColor }}
+        >
+          <h4 className={`review-heading ${review.className}`}>{review.platform}</h4>
+          <p className={review.className}><span className={review.className}>Reviews - </span> 125</p>
+          <p className={review.className}><span className={review.className}>Replied - </span> 120</p>
+          <p className={review.className}><span className={review.className}>Pending - </span> 5</p>
         </div>
+      ))}
       {/* </div> */}
     </>
   );
