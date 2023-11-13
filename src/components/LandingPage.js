c6af6aac39c78cf30f181312baa43b672a1ab109
Uthmanbello <uthmanbello88@gmail.com>
2023-11-14 00:22:25 +0100

    Setup proper routing for landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
       <div className='landing'>
         <h2 className='landing-page-heading'>My Review Partner</h2>
         <div className='lang-btns'>
-          <Link to="/reviews" className='lang-btn english-btn'>Get Started</Link>
+          <Link to="/info" className='lang-btn english-btn'>Get Started</Link>
         </div>    
         <div className='landing-footer'>
           <p style={{ color: '#0F7876', fontWeight: '900' }}>&copy; {new Date().getFullYear()} <span style={{ fontWeight: 900, fontFamily: 'Josefin Sans, sans-serif', color: '#0F7876' }}>DeRoy<span style={{ color: '#F98F45' }} className="deroyale-at">@</span>l&#233;</span></p>
