bf93a10a7bde39b45f142b9b858d9f4ab798d5df
Uthmanbello <uthmanbello88@gmail.com>
2023-11-16 19:02:22 +0100

    Setup chatgpt styling

--- src/components/Chatgpt.js
+++ src/components/Chatgpt.js
@@ -1,103 +1,120 @@
-import React from 'react';
-import { useState } from 'react';
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { ChatContainer, MainContainer, Message, MessageInput, MessageList, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Chatgpt.css';
+import Facebook from './Facebook';
+import FacebookIcon from '../images/facebook.png';
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
-const Chatgpt = () => {
-    const [typing, setTyping] = useState(false)
+const Chatgpt = ({ reviews }) => {
+    const [typing, setTyping] = useState(false);
+    const [currentReviewIndex, setCurrentReviewIndex] = useState(0);
+    const [messages, setMessages] = useState([]);
 
-    const [messages, setMessages] = useState([
-        {
-            message: "Hello, I am the Manager. How can I help you?",
-            sender: "ChatGPT"
+    useEffect(() => {
+        handleSend();
+    }, [reviews]);
+
+    const handleSend = async () => {
+        if (reviews.length === 0 || currentReviewIndex >= reviews.length) {
+            return;
         }
-    ])
 
-    const handleSend = async (message) => {
+        const reviewText = reviews[currentReviewIndex].review_text;
+        const username = reviews[currentReviewIndex].username;
+        const updatedAtString = reviews[currentReviewIndex].updated_at;
+        const updatedAt = new Date(updatedAtString);
+        const formattedDate = updatedAt.toLocaleDateString();
+        const formattedTime = updatedAt.toLocaleTimeString();
+
         const newMessage = {
-            message: message,
+            review: reviewText,
+            username: username,
+            time: formattedTime,
+            date: formattedDate,
             sender: 'user',
-            direction: 'outgoing'
-        }
+        };
 
         const newMessages = [...messages, newMessage];
 
         setMessages(newMessages);
-
         setTyping(true);
 
         await processMessageToChatGPT(newMessages);
-    }
+
+        setCurrentReviewIndex((prevIndex) => prevIndex + 1);
+    };
 
     async function processMessageToChatGPT(chatMessages) {
         let apiMessages = chatMessages.map((messageObject) => {
-            let role = '';
-            if(messageObject.sender === 'ChatGPT') {
-                role = 'assistant'
-            }else{
-                role = 'user'
-            }
-            return { role: role, content: messageObject.message}
+            return { role: messageObject.sender, content: messageObject.review };
         });
 
         const systemMessage = {
             role: 'system',
-            content: 'Speak like a Hotel Manager and answer questions like you manage a multi-puporse hotel.'
-        }
+            content: 'Speak like a Hotel Manager and answer questions like you manage a multi-purpose hotel.',
+        };
 
         const apiRequestBody = {
-            'model': 'gpt-3.5-turbo',
-            'messages': [
-                systemMessage,
-                ...apiMessages
-            ]
-        }
+            model: 'gpt-3.5-turbo',
+            messages: [systemMessage, ...apiMessages],
+        };
 
         await fetch('https://api.openai.com/v1/chat/completions', {
             method: 'POST',
             headers: {
-                'Authorization': 'Bearer ' + apiKey,
-                'Content-Type': 'application/json'
+                Authorization: 'Bearer ' + apiKey,
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(apiRequestBody)
-        }).then((data) => {
-            return data.json();
-        }).then((data) => {
-            console.log(data);
-            setMessages(
-                [...chatMessages, {
-                    message: data.choices[0].message.content,
-                    sender: 'ChatGPT'
-                }]
-            );
-            setTyping(false);
-        });
+            body: JSON.stringify(apiRequestBody),
+        })
+            .then((data) => {
+                return data.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setMessages([
+                    ...chatMessages,
+                    {
+                        response: data.choices[0].message.content,
+                        sender: 'ChatGPT',
+                    },
+                ]);
+                setTyping(false);
+            });
     }
-  return (
-    <>
-      <div className='chat-container-class'>
-        <MainContainer style={{ backgroundColor: 'transparent', border: 'none' }}>
-            <ChatContainer style={{ backgroundColor: 'transparent' }}>
-                <MessageList
-                    style={{ backgroundColor: 'transparent', color: 'green' }}
-                    scrollBehavior='smooth'
-                    typingIndicator={typing ? <TypingIndicator className='typing-indicator' content='Typing...' /> : null }>
-                    {messages.map((message, i) => {
-                        return <Message key={i} model={message} />
-                    })}
-                </MessageList>
-                <MessageInput
-                    style={{ width: '100%', marginTop: '30px', padding: '10px', backgroundColor: '#0c5e5c', color: '#ffffff'}}
-                    placeholder='Type message here' 
-                    onSend={handleSend} />
-            </ChatContainer>
-        </MainContainer>
-      </div>
-    </>
-  );
+
+    return (
+        <>
+            <div className='chat-container-class'>
+                <div className='chat-messages' style={{ height: '75vh', overflowY: 'scroll', padding: '10px' }}>
+                    {messages.map((message, i) => (
+                        <div key={i} className={`message ${message.sender === 'user' ? 'left' : 'right'}`}>
+                            {message.sender === 'user' && <div className='bubble' style={{ textAlign: 'right', margin: '5px'}}>
+                                <div style={{ textAlign: 'right', padding: '10px', display: 'inline-block', borderRadius: '10px', backgroundColor:  '#FDDAC1', color: '#492a13' }}>
+                                    <p style={{ fontSize: '0.7rem' }}>{message.username}</p>
+                                    <p style={{ fontSize: '0.9rem'}}>{message.review}</p>
+                                    <div className='row space-between align-center'>
+                                        <p style={{ fontSize: '0.6rem', marginTop: '-10px' }}>{message.date}</p>
+                                        <img src={FacebookIcon} alt='facebook icon' className='review-icon' /> 
+                                        <p style={{ fontSize: '0.6rem', marginTop: '-10px' }}>{message.time}</p>
+                                    </div>
+                                </div>
+                            </div>}
+                            
+                            {message.sender === 'ChatGPT' && <div className='bubble' style={{ textAlign:'left', margin: '10px', maxWidth: '80%'}}>
+                                <div style={{ fontSize: '0.9rem', padding: '15px', display: 'inline-block', borderRadius: '10px', backgroundColor: '#B0DAD9', color: '#083f3e' }}>
+                                    {message.response}
+                                </div>
+                            </div>}
+                        </div>
+                    ))}
+                </div>
+                <div className='input-container'>
+                    <button onClick={handleSend}>Next Review</button>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Chatgpt;
--- src/components/Tripadvisor.js
+++ src/components/Tripadvisor.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Chatgpt from './Chatgpt';
 
 const Tripadvisor = () => {
 
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:3000/api/v1/facebook_reviews');
+        const data = await response.json();
+        console.log(data)
+        setReviews(data);
+      } catch (error) {
+        console.error('Error fetching Tripadvisor reviews:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <h2 className='page-heading'>TRIPADVISOR</h2>
-      <Chatgpt />
+      <Chatgpt reviews={reviews} />
     </>
   );
 };
