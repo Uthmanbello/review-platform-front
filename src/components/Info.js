936fad755d36a94581fd80a3c17a98e59b544a72
Uthmanbello <uthmanbello88@gmail.com>
2023-11-18 12:16:52 +0100

    Change facebook api in info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -9,7 +9,7 @@ const Info = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:3000/api/v1/facebook_reviews');
+        const response = await fetch('https://facebook-reviews-api.onrender.com/api/v1/facebook_reviews');
         const data = await response.json();
         setReviews(data);
       } catch (error) {
