f0f40f0976c1ec3a75b684f43402461cd4f1f1dd
Uthmanbello <uthmanbello88@gmail.com>
2023-11-25 15:02:57 +0100

    Add loading text to info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,6 +5,7 @@ import '../styles/Info.css';
 
 const Info = () => {
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,8 +13,10 @@ const Info = () => {
         const response = await fetch('https://facebook-reviews-api.onrender.com/api/v1/facebook_reviews');
         const data = await response.json();
         setReviews(data);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching Facebook reviews:', error);
+        setLoading(false);
       }
     };
 
@@ -21,8 +24,8 @@ const Info = () => {
   }, []);
 
   const reviewsData = [
-    { platform: 'FACEBOOK', reviews: `${reviews.length}`, replied: 0, pending: `${reviews.length}`, bgColor: '#FDDAC1', textColor: '#F98F45', className: 'orange' },
-    { platform: 'TRIPADVISOR', reviews: `${data.length}`, replied: 0, pending: `${data.length}`, bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
+    { platform: 'FACEBOOK', reviews: loading ? 'Loading...' : reviews.length, replied: loading ? 'Loading...' : 0, pending: loading ? 'Loading...' : reviews.length, bgColor: '#FDDAC1', textColor: '#F98F45', className: 'orange' },
+    { platform: 'TRIPADVISOR', reviews: loading ? 'Loading...' : data.length, replied: loading ? 'Loading...' : 0, pending: loading ? 'Loading...' : data.length, bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
     { platform: 'BOOKING', reviews: 'N/A', replied: 'N/A', pending: 'N/A', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
     { platform: 'GOOGLE MY BUSINESS', reviews: 'N/A', replied: 'N/A', pending: 'N/A', bgColor: '#B0DAD9', textColor: '#0F7876', className: 'green' },
     { platform: 'EXPEDIA', reviews: 'N/A', replied: 'N/A', pending: 'N/A', bgColor: '#FDDAC1', textColor: '#0F7876', className: 'orange' },
@@ -33,18 +36,18 @@ const Info = () => {
       <h2 className='page-heading'><img src={InfoIcon} alt='info icon' className='heading-icon' />&nbsp;INFO</h2>
 
       <div className='reviews-container'>
-      {reviewsData.map((review, index) => (
-        <div
-          key={index}
-          className='reviews'
-          style={{ backgroundColor: review.bgColor, color: review.textColor }}
-        >
-          <h4 className={`review-heading ${review.className}`}>{review.platform}</h4>
-          <p className={review.className}><span className={review.className}>Reviews - </span> {review.reviews}</p>
-          <p className={review.className}><span className={review.className}>Replied - </span> {review.replied}</p>
-          <p className={review.className}><span className={review.className}>Pending - </span> {review.pending}</p>
-        </div>
-      ))}
+        {reviewsData.map((review, index) => (
+          <div
+            key={index}
+            className='reviews'
+            style={{ backgroundColor: review.bgColor, color: review.textColor }}
+          >
+            <h4 className={`review-heading ${review.className}`}>{review.platform}</h4>
+            <p className={review.className}><span className={review.className}>Reviews - </span> {review.reviews}</p>
+            <p className={review.className}><span className={review.className}>Replied - </span> {review.replied}</p>
+            <p className={review.className}><span className={review.className}>Pending - </span> {review.pending}</p>
+          </div>
+        ))}
       </div>
     </>
   );
